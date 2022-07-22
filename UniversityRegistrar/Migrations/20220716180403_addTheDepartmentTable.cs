667f163c97d50fde745ae11f079177d03c389f36
Danny-ot <oladejidaniel456@gmail.com>
2022-07-22 03:03:24 -0700

    Create the migrations and update database

---
+++ UniversityRegistrar/Migrations/20220716180403_addTheDepartmentTable.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityRegistrar.Migrations
+{
+    public partial class addTheDepartmentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseDepartment",
+                columns: table => new
+                {
+                    CourseDepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseDepartment", x => x.CourseDepartmentId);
+                    table.ForeignKey(
+                        name: "FK_CourseDepartment_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseDepartment_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_DepartmentId",
+                table: "Students",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseDepartment_CourseId",
+                table: "CourseDepartment",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseDepartment_DepartmentId",
+                table: "CourseDepartment",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Department_DepartmentId",
+                table: "Students",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Department_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.DropTable(
+                name: "CourseDepartment");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Students");
+        }
+    }
+}
---
+++ UniversityRegistrar/Migrations/20220717214948_UpdateTheDepartmentJointTables.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityRegistrar.Migrations
+{
+    public partial class UpdateTheDepartmentJointTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartment_Courses_CourseId",
+                table: "CourseDepartment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartment_Department_DepartmentId",
+                table: "CourseDepartment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Department_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Department",
+                table: "Department");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CourseDepartment",
+                table: "CourseDepartment");
+
+            migrationBuilder.RenameTable(
+                name: "Department",
+                newName: "Departments");
+
+            migrationBuilder.RenameTable(
+                name: "CourseDepartment",
+                newName: "CourseDepartments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CourseDepartment_DepartmentId",
+                table: "CourseDepartments",
+                newName: "IX_CourseDepartments_DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CourseDepartment_CourseId",
+                table: "CourseDepartments",
+                newName: "IX_CourseDepartments_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments",
+                column: "DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CourseDepartments",
+                table: "CourseDepartments",
+                column: "CourseDepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartments_Courses_CourseId",
+                table: "CourseDepartments",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartments_Departments_DepartmentId",
+                table: "CourseDepartments",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartments_Courses_CourseId",
+                table: "CourseDepartments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartments_Departments_DepartmentId",
+                table: "CourseDepartments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CourseDepartments",
+                table: "CourseDepartments");
+
+            migrationBuilder.RenameTable(
+                name: "Departments",
+                newName: "Department");
+
+            migrationBuilder.RenameTable(
+                name: "CourseDepartments",
+                newName: "CourseDepartment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CourseDepartments_DepartmentId",
+                table: "CourseDepartment",
+                newName: "IX_CourseDepartment_DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CourseDepartments_CourseId",
+                table: "CourseDepartment",
+                newName: "IX_CourseDepartment_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Department",
+                table: "Department",
+                column: "DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CourseDepartment",
+                table: "CourseDepartment",
+                column: "CourseDepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartment_Courses_CourseId",
+                table: "CourseDepartment",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartment_Department_DepartmentId",
+                table: "CourseDepartment",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Department_DepartmentId",
+                table: "Students",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
